d0db4e5b48e147caa518b176ddc6f077962c6b72
Louis Marais <louis-marais@hotmail.fr>
2024-10-28 22:34:35 +0100

    Styling correction

--- weather-app/src/components/CitySearch.vue
+++ weather-app/src/components/CitySearch.vue
@@ -52,7 +52,7 @@ onMounted(() => {
 </script>
 <template>
     <div class="flex flex-col p-12 h-auto max-w-xl mx-auto">
-        <h2>{{ city }}</h2>
+        <h2 class="text-2xl text-gray-900 dark:text-lime-400">{{ city }}</h2>
         <form @submit.prevent="fetchWeather()">
             <label for="city-search" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">City
                 Search
@@ -61,12 +61,12 @@ onMounted(() => {
                 <input id="city-search" v-model.trim="city" type="text" placeholder="City" @input="fetchCity"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mr-4 col-5">
                 <button
-                    class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                    class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded dark:bg-orange-600 dark:border-orange-800 dark:hover:bg-orange-500  dark:hover:border-orange-700">
                     Search
                 </button>
             </div>
         </form>
-        <ul class="bg-gray-50 dark:bg-gray-700  text-gray-900 dark:text-white">
+        <ul class="bg-gray-50 dark:bg-gray-700  text-gray-900 dark:text-white ">
             <li v-for="suggestion in suggestions" v-bind:key="suggestion.lat">
                 <button v-on:click="fetchWeather(suggestion.lat, suggestion.lon)">
                     <span v-if="suggestion.local_names && suggestion.local_names.fr">
@@ -87,7 +87,7 @@ onMounted(() => {
                 </button>
             </li>
         </ul>
-        <div class="flex flex-row">
+        <div>
             <ul class="flex flex-row" v-if="forecasts">
                 <li v-for="forecast in forecasts" v-bind:key="forecast.dt" class="p-3">
                     <WeatherCard :icon="forecast.weather[0].icon" :dateStr="forecast.dt_txt" :temp="forecast.main.temp"
